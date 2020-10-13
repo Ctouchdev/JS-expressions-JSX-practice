6c87e16790c3c38823cae02deab68f9ca79e0b9e
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-10-13 16:11:57 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Cody";
+const date = new Date();
+const currentYear = date.getFullYear();
+const currentDay = date.getDay();
+console.log(date);
+
+ReactDOM.render(
+  <>
+    <p>Created by: {name}</p>
+    <p>Today is {currentDay}</p>
+    <p>Copyschwads {currentYear}</p>
+  </>,
+  document.getElementById("root")
+);
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
 //Created by YOURNAME.
